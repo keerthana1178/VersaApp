ecd940b6483367b4fedd5ac19ea778d6ffb82896
VT-08 <coderpilaf@gmail.com>
2021-03-04 15:32:38 +0530

    changed gradle version

--- Camera.js
+++ Camera.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 
 const Camera = () => {
+  const [code, setCode] = useState("Point at a Barcode")
   return (
     <View style={styles.container}>
       <Text style={styles.center}>Camera Module</Text>
@@ -13,21 +14,25 @@ const Camera = () => {
         style={styles.preview}
         type={RNCamera.Constants.Type.back}
         flashMode={RNCamera.Constants.FlashMode.on}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
+        // androidCameraPermissionOptions={{
+        //   title: 'Permission to use camera',
+        //   message: 'We need your permission to use your camera',
+        //   buttonPositive: 'Ok',
+        //   buttonNegative: 'Cancel',
+        // }}
+        // androidRecordAudioPermissionOptions={{
+        //   title: 'Permission to use audio recording',
+        //   message: 'We need your permission to use your audio',
+        //   buttonPositive: 'Ok',
+        //   buttonNegative: 'Cancel',
+        // }}
+      
         onGoogleVisionBarcodesDetected={({barcodes}) => {
           console.log(barcodes);
+            setCode(barcodes[0].rawData)
+          
         }}
+        captureAudio={false}
       />
       <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center'}}>
         <TouchableOpacity
@@ -36,7 +41,7 @@ const Camera = () => {
             takePicture;
           }}
           style={styles.capture}>
-          <Text style={{fontSize: 14}}> SNAP </Text>
+          <Text style={{fontSize: 14}}> {code} </Text>
         </TouchableOpacity>
       </View>
     </View>
