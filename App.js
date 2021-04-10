f37c4d692ad8739b9e83b47d7386d86e5e8eff58
nitheesh0299 <76263836+nitheesh0299@users.noreply.github.com>
2021-04-10 14:11:46 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,42 +1,12 @@
-// In index.js of a new project
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import Camera from './Camera';
-// import {Navigation} from 'react-native-navigation';
+import React, {useEffect} from 'react';
+import {View, Text, ActivityIndicator} from 'react-native';
 
-const SettingsScreen = (props) => {
+const App = () => {
   return (
-    <View style={styles.root}>
-      <Camera />
+    <View>
+      <Text>hiuhuifghih</Text>
     </View>
   );
 };
 
-// Navigation.registerComponent('Settings', () => SettingsScreen);
-
-// Navigation.events().registerAppLaunchedListener(async () => {
-//   Navigation.setRoot({
-//     root: {
-//       stack: {
-//         children: [
-//           {
-//             component: {
-//               name: 'Home',
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   });
-// });
-
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'whitesmoke',
-  },
-});
-
-export default SettingsScreen;
+export default App;
