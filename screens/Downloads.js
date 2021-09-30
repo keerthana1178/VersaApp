7aa2d0b36d1e9a48acf4072bdacab89ca2916601
keerthana1178 <67233823+keerthana1178@users.noreply.github.com>
2021-09-30 18:42:38 +0530

    Download.js
    basic download screen. need to connect this to backend to show required data

--- screens/Downloads.js
+++ screens/Downloads.js
@@ -1,21 +1,76 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-
+import React, {useState} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+//import Button from '../components/Button';
+import {Dimensions} from 'react-native';
+import DownloadButton from '../components/DownloadButton';
+import DownloadData from '../components/DownloadData';
 const DownloadScreen = ({navigation}) => {
-    return (
-      <View style={styles.container}>
-        <Text>Download Screen</Text>
-        <Text>Need to work on it.</Text>
-      </View>
-    );
-};
+  const getCurrentDate = () => {
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    return date + '-' + month + '-' + year;
+  };
+  const getCurrentTime = () => {
+    var hours = new Date().getHours();
+    var minutes = new Date().getMinutes();
+    var seconds = new Date().getSeconds();
+    return hours + ':' + minutes + ':' + seconds;
+  };
 
+  const [press, setPress] = useState(false);
+  return (
+    <View style={styles.container}>
+      <View
+        style={{
+          backgroundColor: 'white',
+          width: '100%',
+          height: 200,
+          elevation: 10,
+        }}>
+        <Text style={styles.headerText}>Downloads</Text>
+        <Text style={styles.subText}>Check your download history</Text>
+      </View>
+
+      {press && (
+        <DownloadData date={getCurrentDate()} time={getCurrentTime()} />
+      )}
+      <DownloadButton
+        title="Download"
+        onPress={() => setPress(!press)}></DownloadButton>
+    </View>
+  );
+};
+//position for button bottom nundi pedtunte ravatledhu
 export default DownloadScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    backgroundColor: '#FEF1E6',
+  },
+  header: {
+    backgroundColor: 'white',
+    paddingTop: 50,
+    elevation: 10,
+  },
+  headerText: {
+    backgroundColor: 'white',
+    position: 'relative',
+    fontFamily: 'Raleway',
+    fontSize: 40,
+    fontWeight: '700',
+    top: 66,
+    textAlign: 'center',
+  },
+  subText: {
+    backgroundColor: 'white',
+    position: 'relative',
+    fontFamily: 'Raleway',
+    fontSize: 18,
+    color: '#909090',
+    fontWeight: '300',
+    top: 66,
+    textAlign: 'center',
   },
 });
