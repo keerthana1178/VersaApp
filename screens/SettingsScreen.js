1b24f7be7c666dd3fad6584cc11a047edad388d5
keerthana1178 <67233823+keerthana1178@users.noreply.github.com>
2021-11-30 11:16:06 +0530

    Updates settings screen
    errors in navigation in settings screen are fixed

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,71 +1,81 @@
 import React from 'react';
 
-import { View, Text, Button, StyleSheet,SafeAreaView,ScrollView,TouchableOpacity,Alert } from 'react-native';
+import {
+  View,
+  Text,
+  Button,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 import {useTheme} from '@react-navigation/native';
 
-
 const SettingsScreen = ({navigation}) => {
-    const {colors} = useTheme();
-    const theme = useTheme();
-    return (
-      
-      <SafeAreaView style={styles.container}>
+  const {colors} = useTheme();
+  const theme = useTheme();
+  return (
+    <SafeAreaView style={styles.container}>
       <ScrollView>
         <View style={styles.header}>
           <Text style={styles.h1}>Settings</Text>
         </View>
-        <TouchableOpacity onPress={()=>{navigation.push('Accounts')}}>
-        <Text style={styles.h2}>Account Details</Text>  
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Accounts');
+          }}>
+          <Text style={styles.h2}>Account Details</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>{navigation.push('Date')}}>
-        <Text style={styles.h2}>Date & Time</Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate('Date');
+          }}>
+          <Text style={styles.h2}>Date & Time</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={()=>{Alert.alert("Clear Data","Do you want to erase all the data?",
-        [
-          {text:"yes"},
-          {text:"No"},
-        ])
-        }}>
-        <Text style={styles.h2}>Clear All Data</Text>
+        <TouchableOpacity
+          onPress={() => {
+            Alert.alert('Clear Data', 'Do you want to erase all the data?', [
+              {text: 'yes'},
+              {text: 'No'},
+            ]);
+          }}>
+          <Text style={styles.h2}>Clear All Data</Text>
         </TouchableOpacity>
         <TouchableOpacity>
-        <Text style={styles.h2}>Help</Text>
+          <Text style={styles.h2}>Help</Text>
         </TouchableOpacity>
-        </ScrollView>  
-      </SafeAreaView>
-      
-    );
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default SettingsScreen;
 
-
-
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
+    flex: 1,
   },
-  header:{
-    flex:1,
-    alignContent:'center',
-    justifyContent:'center',
-    backgroundColor:'white',
-    elevation:10,
-    width:'100%',
-    height:200
+  header: {
+    flex: 1,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    elevation: 10,
+    width: '100%',
+    height: 200,
   },
-  h1:{
-    fontSize:28,
-    padding:40,
-    fontWeight:'bold',
-    textDecorationStyle:'solid',
-    textDecorationLine:'underline',
+  h1: {
+    fontSize: 28,
+    padding: 40,
+    fontWeight: 'bold',
+    textDecorationStyle: 'solid',
+    textDecorationLine: 'underline',
   },
-  h2:{
-    fontSize:18,
-    padding:20,
-    paddingLeft:40,
-    fontWeight:'bold'
+  h2: {
+    fontSize: 18,
+    padding: 20,
+    paddingLeft: 40,
+    fontWeight: 'bold',
   },
 });
